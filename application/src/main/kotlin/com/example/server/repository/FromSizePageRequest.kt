df4903e61fc78e79924c44b777ab66aaa01f3736
Козлов Никита Юрьевич <saintduder@gmail.com>
2025-04-28 21:33:30 +0300

    refactor: добавить небольшие изменения

--- application/src/main/kotlin/com/example/server/repository/FromSizePageRequest.kt
+++ application/src/main/kotlin/com/example/server/repository/FromSizePageRequest.kt
@@ -3,8 +3,10 @@ package com.example.server.repository
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 
-class FromSizePageRequest private constructor(page: Int, size: Int, sort: Sort) :
-    PageRequest(page, size, sort) {
+class FromSizePageRequest private constructor(
+    page: Int, size: Int, sort: Sort
+) : PageRequest(page, size, sort) {
+
     companion object {
         @JvmStatic
         @JvmOverloads
